ce2283a40e0595f5ef8b69c9d1f099733e32567d
LucasCarlos7 <lukascarlos7777@gmail.com>
2024-06-04 21:41:24 -0300

    Refatorando o código

--- src/main/java/br/com/senac/financasjpa/FinancasJPA.java
+++ src/main/java/br/com/senac/financasjpa/FinancasJPA.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.com.senac.financasjpa;
 
-/**
- *
- * @author Lucas
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.time.LocalDate;
+
 public class FinancasJPA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        EntityManagerFactory fabricaEntidade = Persistence.createEntityManagerFactory("Financas-PU");
+        EntityManager manager = fabricaEntidade.createEntityManager();
+        
+        Despesa gasto = new Despesa();
+        gasto.setDescricao("Primeira despesa");
+        gasto.setValor(10.50);
+        gasto.setData(LocalDate.of(2024, 6, 4));
+        
+        manager.getTransaction().begin();
+        manager.persist(gasto);
+        manager.getTransaction().commit();
+        
+        manager.close();
+        fabricaEntidade.close();
     }
 }
