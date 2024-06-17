3e96787c9c96d8d3eec802b61b5f4c0ea3f3a11a
LucasCarlos7 <lukascarlos7777@gmail.com>
2024-06-17 19:31:56 -0300

    criando a tela de cadastro de despesa

--- src/main/java/br/com/senac/financasjpa/FinancasJPA.java
+++ src/main/java/br/com/senac/financasjpa/FinancasJPA.java
@@ -8,13 +8,5 @@ public class FinancasJPA {
 
     public static void main(String[] args) {
 
-        DespesaDAO despesaDao = new DespesaDAO();
-
-        Despesa d = new Despesa();
-        d.setDescricao("Compras de mercado");
-        d.setValor(165.70);
-        d.setData(LocalDate.of(2022, 11, 15));
-
-        despesaDao.cadastrar(d);
     }
 }
